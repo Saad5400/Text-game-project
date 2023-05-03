e1fb251fb74a2698b4024d04897c74efa52d015c
Saad5400 <sdbtwa@gmail.com>
2023-05-03 08:57:25 +0300

    created class room constructer that will construct all the rooms

---
+++ src/main/java/edu/uqu/cs/RoomsConstructer.java
@@ -0,0 +1,60 @@
+package edu.uqu.cs;
+
+import java.util.ArrayList;
+
+import edu.uqu.cs.rooms.*;
+import edu.uqu.cs.characters.*;
+
+public class RoomsConstructer {
+
+    public ArrayList<Room> rooms = new ArrayList<Room>();
+
+    public RoomsConstructer() {
+        rooms.add(createRoomStart());
+        rooms.add(createRoomGarden());
+    }
+
+    public Room createRoomStart() {
+        Room roomStart = new Room();
+        roomStart.setMessage("You just woke up in a dark and empty castle!");
+        roomStart.setOptions(
+            new Option("Go out", () -> {
+
+                System.out.println("You went out...");
+                createRoomGarden().start();
+
+            }),
+            new Option("Explore the castle", () -> {
+
+                System.out.println("You are exploring the castle...");
+
+            })
+        );
+        roomStart.setEnemies(
+            new Enemy("Enemy 1", 10.0, 10.0),
+            new Enemy("Enemy 2", 10.0, 10.0)
+        );
+
+        return roomStart;
+    }
+
+    public Room createRoomGarden() {
+        Room roomGarden = new Room();
+        roomGarden.setMessage("You are out in the garden!");
+        roomGarden.setOptions(
+            new Option("Go back", () -> {
+
+                System.out.println("You went back...");
+                createRoomStart().start();
+
+            }),
+            new Option("Fight the skeleton", () -> {
+
+                System.out.println("Fight started...");
+
+            })
+        );
+
+        return roomGarden;
+    }
+}
