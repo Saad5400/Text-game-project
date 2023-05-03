817712fdcfee5bb4572a89e097ac5676ae68a90a
Saad5400 <sdbtwa@gmail.com>
2023-05-03 08:57:04 +0300

    now just call room constructer to get all the rooms and start the first one

--- src/main/java/edu/uqu/cs/App.java
+++ src/main/java/edu/uqu/cs/App.java
@@ -1,38 +1,20 @@
 package edu.uqu.cs;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
-import edu.uqu.cs.rooms.Option;
 import edu.uqu.cs.rooms.Room;
 
 public class App {
 
     public static Scanner scanner = new Scanner(System.in);
 
-    public static <T> ArrayList<T> asList(T... a) {
-        return new ArrayList<>(Arrays.asList(a));
-    }
-
     public static void main(String[] args) {
 
         /* Hint use this as your driver program */
-        Room room1 = new Room(
-            "You just woke up",
-            asList(
-                new Option("Go out", () -> {
-                    System.out.println("You went out");
-                }),
-                new Option("go ahead", () -> {
-                    System.out.println("hi");
-                })
-            ),
-            null
-        );
+        ArrayList<Room> rooms = new RoomsConstructer().rooms;
 
-        room1.start();
+        rooms.get(0).start();
     }
 
 }
