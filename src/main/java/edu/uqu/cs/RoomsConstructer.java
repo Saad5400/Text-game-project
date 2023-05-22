5d0a56ccb02b9e36806d26a4da5ed9ff8bb633e8
rayanAdeb <120191811+rayanAdeb@users.noreply.github.com>
2023-05-22 10:15:48 +0000

    added simple fight idea

--- src/main/java/edu/uqu/cs/RoomsConstructer.java
+++ src/main/java/edu/uqu/cs/RoomsConstructer.java
@@ -1,6 +1,9 @@
 package edu.uqu.cs;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 import edu.uqu.cs.Utilities.AnsiColor;
 import edu.uqu.cs.characters.*;
 
@@ -30,27 +33,26 @@ public class RoomsConstructer {
 
         // set each option and its action
         roomStart.setOptions(
-            // option 1               // this is a lambda expression that will be called when the user choose this option
-            new Option("Go out", () -> {
+                // option 1 // this is a lambda expression that will be called when the user
+                // choose this option
+                new Option("Go out", () -> {
 
-                System.out.println("You went out...");
-                // you can change rooms like this
-                createRoomGarden().start();
+                    System.out.println("You went out...");
+                    // you can change rooms like this
+                    createRoomGarden().start();
 
-            }),
-            // option 2                          
-            new Option("Explore the castle", () -> {
+                }),
+                // option 2
+                new Option("Explore the castle", () -> {
 
-                System.out.println("You are exploring the castle...");
-                exploreTheCastle().start();
-            })
-        );
+                    System.out.println("You are exploring the castle...");
+                    exploreTheCastle().start();
+                }));
         // this is useless for now
         roomStart.setEnemies(
-            new Enemy("Enemy 1", 10.0, 10.0, 5.0, 5.0),
-            new Enemy("Enemy 2", 10.0, 10.0, 5.0,5.0)
-        );
-        
+                new Enemy("Enemy 1", 10.0, 10.0, 5.0, 5.0),
+                new Enemy("Enemy 2", 10.0, 10.0, 5.0, 5.0));
+
         return roomStart;
     }
 
@@ -64,48 +66,72 @@ public class RoomsConstructer {
         // IMPORTANT: No more set message, use set event like above
 
         roomGarden.setOptions(
-            new Option("Go back", () -> {
+                new Option("Go back", () -> {
 
-                System.out.println("You went back...");
-                createRoomStart().start();
+                    System.out.println("You went back...");
+                    createRoomStart().start();
 
-            }),
-            new Option("Fight the skeleton", () -> {
+                }),
+                new Option("Fight the skeleton", () -> {
 
-                System.out.println("Fight started...");
+                    App.player.health -= 10;
+                    if (App.player.health <= 0) {
 
-            })
-        );
+                    }
+                    Enemy enemy = new Enemy("Skeleton", 100.0, 40.0);
+                    enemy.health -= App.player.attackDamage;
+
+                    Utilities.print("Fight started...\n");
+                    Random random = new Random();
+                    int answer = random.nextInt(3) + 1;
+                    Scanner scnr = new Scanner(System.in);
+
+                    for(int i =0; i<3; i++){
+                        Utilities.print("You should enter a number between 1 to 3: ", AnsiColor.RED);
+                        int input = scnr.nextInt();
+
+                        if(input == answer){
+                            Utilities.print("your damage the skeleton");
+                        }
+                        else if (input != answer){
+                            Utilities.print("your damaged\n",AnsiColor.RED);
+                        }  
+                        
+                        else{
+
+                        }
+
+                    }
+
+
+                }));
 
         return roomGarden;
     }
-    public Room exploreTheCastle(){
+
+    public Room exploreTheCastle() {
         Room room = new Room();
 
         room.setEvent(() -> {
             Utilities.print("You are in the castle!\n\n", AnsiColor.GREEN);
         });
-        //room.setMessage("You are in the castle");
+        // room.setMessage("You are in the castle");
         // IMPORTANT: No more set message, use set event like above
 
-        
         room.setOptions(
-            // option 1
-            new Option("Go back", () -> {
+                // option 1
+                new Option("Go back", () -> {
 
-                System.out.println("You went back...");
-                createRoomGarden().start();
+                    System.out.println("You went back...");
+                    createRoomGarden().start();
 
-            }),
-            // option 2
-            new Option("Go ahead", () -> {
+                }),
+                // option 2
+                new Option("Go ahead", () -> {
 
-                System.out.println("You going ahead...");
-                
+                    System.out.println("You going ahead...");
 
-            })
-        );
-
+                }));
 
         return room;
     }
