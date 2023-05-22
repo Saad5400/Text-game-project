7329240e98f862ce22377bcc7fc985dfa3000d59
rayanAdeb <120191811+rayanAdeb@users.noreply.github.com>
2023-05-22 10:15:38 +0000

    added method overload

--- src/main/java/edu/uqu/cs/characters/Enemy.java
+++ src/main/java/edu/uqu/cs/characters/Enemy.java
@@ -9,6 +9,14 @@ public class Enemy extends Character {
         this.critRate = critRate;
         this.critdamage = critdamage;
     }
+    public Enemy(String name, Double health, Double attackDamage) {
+        // Enemy Stats is deffernt depneds on level
+        this.name = name;
+        this.health = health;
+        this.attackDamage = attackDamage;
+        this.critRate = 0.0;
+        this.critdamage = 0.0;
+    }
 
     // Die Message
     public void die() {
