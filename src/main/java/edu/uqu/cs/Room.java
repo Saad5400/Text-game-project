e786e391a97a3c88a7d13dd0fbba29e906f80d1c
JawadAlharbi <120259575+JawadAlharbi@users.noreply.github.com>
2023-05-23 10:51:17 +0000

    Javadoc has been add

--- src/main/java/edu/uqu/cs/Room.java
+++ src/main/java/edu/uqu/cs/Room.java
@@ -9,93 +9,172 @@ public class Room {
 
     // for each room object we will store its message, options and enemies
     // see class Option and class Enemy
-    private Runnable event = () -> {}; // empty event
-    private ArrayList<Option> options = new ArrayList<Option>();
+    /**
+     * 
+     * This class represents a room in a video game.
+     * 
+     * @author Bard
+     */
+    public class Room {
 
-    // this will be used to store the user choice
-    private int playerChoice;
+        /**
+         * 
+         * The event that will be run when the room starts.
+         */
+        private Runnable event;
+        /**
+         * 
+         * The list of options available in the room.
+         */
+        private ArrayList<Option> options;
+        /**
+         * 
+         * The user's choice of option.
+         */
+        private int playerChoice;
 
-    //#region Constructors
-    public Room(ArrayList<Option> options, ArrayList<Enemy> enemies, Runnable event) {
-        this.options = options;
-        this.event = event;
-    }
-
-    public Room(ArrayList<Option> options, ArrayList<Enemy> enemies) {
-        this(options, enemies, () -> {});
-    }
-
-    public Room(ArrayList<Option> options) {
-        this(options, new ArrayList<Enemy>());
-    }
-
-    public Room() {
-        this(new ArrayList<Option>());
-    }
-    //#endregion Constructors
-
-    // this method will be called to start the room
-    public void start() {
-
-        // first print the message with white color using the print method in Utilities class
-        // it's very similiar to printf, but it will print in colors and add a typing effect
-        // see Utilities.java
-        event.run();
-        // loop for each option and print its text
-        for (int i = 0; i < options.size(); i++) {
-            Utilities.print((i+1) + ". " + options.get(i).text, AnsiColor.CYAN);
-            Utilities.print();
+        /**
+         * 
+         * Creates a new room with an empty event and no options.
+         */
+        public Room() {
+            this(new ArrayList<>());
         }
-        // printing a new line
-        Utilities.print();
 
-        // now we will get the user choice, make sure it's valid and then run the action
-        while (true) {
+        /**
+         * 
+         * Creates a new room with the specified event and no options.
+         * 
+         * @param event The event that will be run when the room starts.
+         */
+        public Room(Runnable event) {
+            this(new ArrayList<>(), event);
+        }
 
-            Utilities.print("Enter your choice: ", AnsiColor.BLUE);
+        /**
+         * 
+         * Creates a new room with the specified options and no event.
+         * 
+         * @param options The list of options available in the room.
+         */
+        public Room(ArrayList<Option> options) {
+            this(options, new ArrayList<>());
+        }
 
-            try {
-                // see Utilities.java
-                Utilities.setColor(AnsiColor.PURPLE);
-                playerChoice = App.scanner.nextInt();
-                Utilities.resetColor();
+        /**
+         * 
+         * Creates a new room with the specified options and event.
+         * 
+         * @param options The list of options available in the room.
+         * @param event   The event that will be run when the room starts.
+         */
+        public Room(ArrayList<Option> options, Runnable event) {
+            this.options = options;
+            this.event = event;
+        }
 
+        /**
+         * 
+         * Starts the room.
+         * 
+         * This method will print the room's message, list the available options, and
+         * get the user's choice.
+         * 
+         * The action of the chosen option will then be run.
+         */
+
+        private Runnable event = () -> {
+        }; // empty event
+        private ArrayList<Option> options = new ArrayList<Option>();
+
+        // this will be used to store the user choice
+        private int playerChoice;
+
+        // #region Constructors
+        public Room(ArrayList<Option> options, ArrayList<Enemy> enemies, Runnable event) {
+            this.options = options;
+            this.event = event;
+        }
+
+        public Room(ArrayList<Option> options, ArrayList<Enemy> enemies) {
+            this(options, enemies, () -> {
+            });
+        }
+
+        public Room(ArrayList<Option> options) {
+            this(options, new ArrayList<Enemy>());
+        }
+
+        public Room() {
+            this(new ArrayList<Option>());
+        }
+        // #endregion Constructors
+
+        // this method will be called to start the room
+        public void start() {
+
+            // first print the message with white color using the print method in Utilities
+            // class
+            // it's very similiar to printf, but it will print in colors and add a typing
+            // effect
+            // see Utilities.java
+            event.run();
+            // loop for each option and print its text
+            for (int i = 0; i < options.size(); i++) {
+                Utilities.print((i + 1) + ". " + options.get(i).text, AnsiColor.CYAN);
                 Utilities.print();
+            }
+            // printing a new line
+            Utilities.print();
 
-                // validate the user choice
-                if (playerChoice > options.size() || playerChoice <= 0){
-                    Utilities.print("Your choice must be betweem 1 and " + options.size() + "!\n\n", AnsiColor.RED);
-                }
-                else {
-                    // if the choice is valid, break the loop
-                    break;
+            // now we will get the user choice, make sure it's valid and then run the action
+            while (true) {
+
+                Utilities.print("Enter your choice: ", AnsiColor.BLUE);
+
+                try {
+                    // see Utilities.java
+                    Utilities.setColor(AnsiColor.PURPLE);
+                    playerChoice = App.scanner.nextInt();
+                    Utilities.resetColor();
+
+                    Utilities.print();
+
+                    // validate the user choice
+                    if (playerChoice > options.size() || playerChoice <= 0) {
+                        Utilities.print("Your choice must be betweem 1 and " + options.size() + "!\n\n", AnsiColor.RED);
+                    } else {
+                        // if the choice is valid, break the loop
+                        break;
+                    }
+                } catch (Exception e) {
+                    System.out.print(AnsiColor.RED);
+                    System.out.println("\nInvalid input!\n");
+                    App.scanner.nextLine();
                 }
             }
-            catch (Exception e) {
-                System.out.print(AnsiColor.RED);
-                System.out.println("\nInvalid input!\n");
-                App.scanner.nextLine();
-            }
+            Utilities.resetColor();
+            // run the action of the chosen option
+            options.get(playerChoice - 1).action.run();
         }
-        Utilities.resetColor();
-        // run the action of the chosen option
-        options.get(playerChoice-1).action.run();
-    }
 
-    //#region Getters and Setters
+        // #region Getters and Setters
 
-    public ArrayList<Option> getOptions() {
-        return options;
-    }
-    public void setOptions(ArrayList<Option> options) {
-        this.options = options;
-    }
-    public void setOptions(Option... options) {
-        this.options = Utilities.asList(options);
-    }
+        public ArrayList<Option> getOptions() {
+            return options;
+        }
 
-    public void setEvent(Runnable event){
-        this.event = event;
+        public void setOptions(ArrayList<Option> options) {
+            this.options = options;
+        }
+
+        public void setOptions(Option... options) {
+            this.options = Utilities.asList(options);
+        }
+
+        public void setEvent(Runnable event) {
+            this.event = event;
+        }
+        // #endregion Getters and Setters
     }
-    //#endregion Getters and Setters
-} 
+}
--- src/main/java/edu/uqu/cs/Utilities.java
+++ src/main/java/edu/uqu/cs/Utilities.java
@@ -4,8 +4,90 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Utilities {
+/**
+ * This class provides a variety of utility methods for printing text, setting colors, and sleeping.
+ *
+ * @author Saad
+ */
+public class Utilities {
 
+    /**
+     * An enumeration of all the colors that can be used in the console.
+     */
+    public enum AnsiColor {
+        RESET("\u001B[0m"),
+        BLACK("\u001B[30m"),
+        RED("\u001B[31m"),
+        GREEN("\u001B[32m"),
+        YELLOW("\u001B[33m"),
+        BLUE("\u001B[34m"),
+        PURPLE("\u001B[35m"),
+        CYAN("\u001B[36m"),
+        WHITE("\u001B[37m"),
+        BLACK_BACKGROUND("\u001B[40m"),
+        RED_BACKGROUND("\u001B[41m"),
+        GREEN_BACKGROUND("\u001B[42m"),
+        YELLOW_BACKGROUND("\u001B[43m"),
+        BLUE_BACKGROUND("\u001B[44m"),
+        PURPLE_BACKGROUND("\u001B[45m"),
+        CYAN_BACKGROUND("\u001B[46m"),
+        WHITE_BACKGROUND("\u001B[47m"),
+        BOLD("\u001B[1m"),
+        UNDERLINE("\u001B[4m"),
+        ITALIC("\u001B[3m"),
+        STRIKETHROUGH("\u001B[9m"),
+        BLINK("\u001B[5m"),
+        REVERSED("\u001B[7m"),
+        INVISIBLE("\u001B[8m"),
+        FRAMED("\u001B[51m"),
+        ENCIRCLED("\u001B[52m"),
+        OVERLINED("\u001B[53m"),
+        BRIGHT_BLACK("\u001B[90m"),
+        BRIGHT_RED("\u001B[91m"),
+        BRIGHT_GREEN("\u001B[92m"),
+        BRIGHT_YELLOW("\u001B[93m"),
+        BRIGHT_BLUE("\u001B[94m"),
+        BRIGHT_PURPLE("\u001B[95m"),
+        BRIGHT_CYAN("\u001B[96m"),
+        BRIGHT_WHITE("\u001B[97m"),
+        BRIGHT_BLACK_BACKGROUND("\u001B[100m"),
+        BRIGHT_RED_BACKGROUND("\u001B[101m"),
+        BRIGHT_GREEN_BACKGROUND("\u001B[102m"),
+        BRIGHT_YELLOW_BACKGROUND("\u001B[103m"),
+        BRIGHT_BLUE_BACKGROUND("\u001B[104m"),
+        BRIGHT_PURPLE_BACKGROUND("\u001B[105m"),
+        BRIGHT_CYAN_BACKGROUND("\u001B[106m"),
+        BRIGHT_WHITE_BACKGROUND("\u001B[107m");
+
+        private final String code;
+
+        AnsiColor(String code) {
+            this.code = code;
+        }
+
+        /**
+         * Gets the ANSI escape code for this color.
+         *
+         * @return The ANSI escape code for this color.
+         */
+        public String getCode() {
+            return code;
+        }
+    }
+
+    // default delay between printing characters
+    public static final int DEFAULT_DELAY = 25;
+
+    /**
+     * Prints a string with a delay between each character.
+     *
+     * @param s The string to print.
+     * @param color The color to use.
+     * @param delay The delay between characters, in milliseconds.
+     * @param args The arguments to format the string.
+     */
     // all the colors that can be used in the console
+
     public static class AnsiColor {
         public static final String RESET = "\u001B[0m";
         public static final String BLACK = "\u001B[30m";
@@ -104,4 +186,5 @@ public class Utilities {
     public static <T> ArrayList<T> asList(T... a) {
         return new ArrayList<T>(Arrays.asList(a));
     }
+}
 }
